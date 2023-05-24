c28449a118a70bf6d56915c5a11cfadcbf5391e6
Jason Henley <jasonhenley23@gmail.com>
2023-05-24 18:59:55 -0500

    hi

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.json())
+
+const PORT = process.env.PORT || 3000
+const express = require('express');
+const server = express();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
